384e27638208dec50201c1ae1767f915e17fb255
Leonardo_Cerce <leonardo.cerce@gmail.com>
2020-03-26 17:40:45 -0300

    Comentários adicionados

--- exemplo_mkdir.c
+++ exemplo_mkdir.c
@@ -1,3 +1,12 @@
+/*
+* Esse programa exemplifica o uso da syscall
+* 
+* mkdir(const char* pathname, mode_t mode).
+*
+* Ao executar o programa, ele tenta criar
+* um diretorio chamado "Pasta" no diretorio de trabalho.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -11,11 +20,11 @@ int main()
 
     err = mkdir("Pasta");
 
-    if(err == ERRO)
+    if(err == ERRO) // se err eh -1, significa que nao foi possivel criar a pasta
     {
         printf("Erro ao criar pasta.");
         return ERRO;
     }
 
-    return SUCESSO;
+    return SUCESSO; // senao, foi criado com sucesso
 }
--- exemplo_open_close_write.c
+++ exemplo_open_close_write.c
@@ -1,3 +1,17 @@
+/*
+* Esse programa exemplifica o uso das syscalls
+*
+* open(const char* pathname, int flags, mode_t mode)
+*
+* read(int fd, const void *buf, size_t count)
+*
+* close(int fd)
+*
+* Ao executar o programa, ele cria um arquivo chamado Arquivo de extensao
+* txt, escreve "Hello" e depois fecha o arquivo. O arquivo fica salvo no
+* diretorio de trabalho.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -11,23 +25,23 @@ int main()
     FILE * fp;
     int err;
 
-    fp = (FILE *) fopen("Arquivo.txt", "wt");
+    fp = (FILE *) fopen("Arquivo.txt", "wt"); // cria um arquivo para escrita
 
     if(fp == NULL)
     {
-        printf("Erro ao abrir arquivo.");
+        printf("Erro ao abrir arquivo."); // se o descritor for nulo, entao houve erro na criacao
         return ERRO;
     }
 
-    err = fwrite("Hello", 1, TAM, fp);
+    err = fwrite("Hello", 1, TAM, fp); // escreve Hello no arquivo
 
     if(err != TAM)
     {
-        printf("Erro ao escrever.");
+        printf("Erro ao escrever."); // se o numero de caracteres for diferente, entao houve erro
         return ERRO;
     }
 
-    fclose(fp);
+    fclose(fp); // fecha o arquivo
 
     return SUCESSO;
 }
